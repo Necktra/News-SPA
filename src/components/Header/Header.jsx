b4bcd60ced828821a473ea5dd89ef1ff53d289b7
Ira <hallo.ween@mail.ru>
2021-09-09 16:26:02 +0300

    add logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import { Container, Navbar } from 'react-bootstrap';
 import classes from './Header.module.css';
 
+import { ReactComponent as HeaderLogo } from './../../assets/images/headerLogo.svg';
+
 const Header = (props) => {
     return (
         <Navbar bg="dark" variant="dark" className={classes.navbarWrapper}>
             <Container className={classes.logoContainer}>
-                <Navbar.Brand href="/"><span className={classes.logo}>HACKER NEWS</span></Navbar.Brand>
+                <Navbar.Brand href="/"><HeaderLogo className={classes.logo}/></Navbar.Brand>
             </Container>
         </Navbar>
     )
